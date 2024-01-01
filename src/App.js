d5054c40dc6e88a4cd7ca0e567be516dfd747c41
Armeen Sultana <armeensultana.bscssef20@iba-suk.edu.pk>
2024-01-01 09:31:03 +0500

    comment added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import { useState } from "react";
+import { CartProducts } from "./hooks/hooks";
+
+function App() {
+  const [cart, setCart] = useState([]);
+  return (
+    <>
+      <CartProducts.Provider value={{ cart, setCart }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        {/* change */}
+        <Footer />
+      </CartProducts.Provider>
+    </>
+  );
+}
+
+export default App;
